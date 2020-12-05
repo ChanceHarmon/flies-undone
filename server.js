8b2276ca89d5ee9a36e36eb31065a7bd39473689
Chance Harmon <henrikillsit@gmail.com>
2020-12-04 23:34:19 -0800

    siignifiicant gains as far as data mapping and flow, getting closer to a new data set

--- server.js
+++ server.js
@@ -26,36 +26,74 @@ app.post('/search', handleSearch);
 //This below function works as a reference for grabing image urls, want to keep it for now.
 
 function handleSearch(request, response) {
-  console.log('made it', request.body)
+
+
+  //check list
+  //image = done
+  //description = done
+  //imitate = not
+  //species = not
+  //more = not done
+
+  //console.log('made it', request.body)
   // let temp = [];
 
 
-  // superagent.get('https://www.bigyflyco.com/items/Flies/Dry-Flies/list.htm')
-  //   .then(result => {
+  superagent.get('https://www.bigyflyco.com/adams-detail.htm')
+    .then(result => {
 
-  //     const $ = cheerio.load(result.text)
-  //     //console.log($('#imgMainImage'))
-  //     const stepOne = $('#imgMainImage')
-  //     const stepTwo = Object.entries(stepOne)
-  //     stepTwo.forEach((item, i) => {
-  //       console.log('in foreach', item)
-  //       if (item[1].attribs) {
-  //         temp.push({ dryFlyUrl: item[1].attribs['data-cfsrc'] });
-  //         //temp.push(item[1].attribs['data-cfsrc']);
-  //       }
-  //     })
-  //     let testVar = { data: { dryFlyPageOne: temp } }
-  //     let otherVar = JSON.stringify(testVar)
-  //     fs.writeFile('data/dry-flies/page-1.json', otherVar, (err) => {
-  //       if (err) throw err;
-  //     })
+      const $ = cheerio.load(result.text)
+      const stepOne = $('#imgMainImage')
+      const stepTwo = $('[itemprop ="description"]')
+      const image = stepOne['0'].attribs['data-cfsrc'];
+      let descripStr = '';
 
-  //     // fs.writeFile('test3.json', temp, (err) => {
-  //     //   if (err) throw err;
-  //     // })
-  //   })
+      //keep this for now
+      //console.log('stepTwo', stepTwo['0'].children[4].children[1].children[0].children[0].data)//this works for a single li for description
 
-  //   .catch(err => console.error(err))
+      //also  stepTwo['0'].children[4] I think that index of 5 or 6 is the next step for the other props I want, so keep track of that for future reference 
+
+      //this step is store the li strings
+
+      stepTwo['0'].children[4].children.forEach(child => {
+        if (child.name) {
+          if (child.name === 'li') {
+            if (child.children[0].children[0].data) {
+              descripStr += child.children[0].children[0].data
+            }
+          }
+        }
+      })
+      //console.log('string', descripStr)
+      //Don't touch above, works perfectly!!!!!!
+
+
+      //next step, get that nested block info of imitate, fish, and more. Let's go !!!!!
+
+
+
+      //     const stepTwo = Object.entries(stepOne)
+      //     stepTwo.forEach((item, i) => {
+      //       console.log('in foreach', item)
+      //       if (item[1].attribs) {
+      //         temp.push({ dryFlyUrl: item[1].attribs['data-cfsrc'] });
+      //         //temp.push(item[1].attribs['data-cfsrc']);
+      //       }
+      //     })
+      //     let testVar = { data: { dryFlyPageOne: temp } }
+      //     let otherVar = JSON.stringify(testVar)
+      //     fs.writeFile('data/dry-flies/page-1.json', otherVar, (err) => {
+      //       if (err) throw err;
+      //     })
+
+      //     // fs.writeFile('test3.json', temp, (err) => {
+      //     //   if (err) throw err;
+      //     // })
+    }).then(() => {
+      response.redirect('/')
+    })
+
+    .catch(err => console.error(err))
 }
 
 
